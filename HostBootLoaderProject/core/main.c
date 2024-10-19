9e468763653dc5dd87dd42333f8f0f3ec3e9b82d
Adrian Nwajuobi <adrian.nwajuobi@gmail.com>
2024-10-19 17:24:21 -0400

    Added more functionality to the boot loader Host Client to enable commands to go to an applicaion address in flash and to erase an application in flash.

--- HostBootLoaderProject/core/main.c
+++ HostBootLoaderProject/core/main.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "comms_device.h"
 #include "BootProtocolDefs.h"
 
@@ -46,9 +47,15 @@ static void host_bl_get_version (comms_device_t commsDevice);
 static void host_bl_get_help (comms_device_t commsDevice);
 static void bl_get_chip_id (comms_device_t commsDevice);
 static void bl_get_rdp_status (comms_device_t commsDevice);
+static void bl_go_to_app_address (comms_device_t commsDevice);
+static void bl_flash_erase (comms_device_t commsDevice);
 
-static void (*boot_loader_operations[5]) (comms_device_t commsDevice) = {0, host_bl_get_version, host_bl_get_help,
-		bl_get_chip_id, bl_get_rdp_status};
+static bool run_preamble_sequence (comms_device_t commsDevice);
+static bool run_protocol_packet_sequence (comms_device_t commsDevice, uint8_t * packet,
+		uint32_t packet_size, uint8_t * crcResponse, uint32_t crcResponseSize);
+
+static void (*boot_loader_operations[7]) (comms_device_t commsDevice) = {0, host_bl_get_version, host_bl_get_help,
+		bl_get_chip_id, bl_get_rdp_status, bl_go_to_app_address, bl_flash_erase};
 
 
 int main (int argc, char * argv[]) {
@@ -91,26 +98,15 @@ static void run_boot_loader_logic (comms_device_t commsDevice, int32_t operation
 static void bl_get_rdp_status (comms_device_t commsDevice) {
 	uint8_t bl_get_rdp_status_pkt[6];
 	uint8_t crcResponse[2];
-	uint8_t ackPreambleResponse = 0;
 	uint8_t mcu_flash_rdp_status = 0;
 	bl_get_rdp_status_pkt[0] = 5;
 	bl_get_rdp_status_pkt[1] = BL_GET_RDP_STATUS;
 	uint32_t * crcPortionPtr = (uint32_t *)(bl_get_rdp_status_pkt+2);
 	*crcPortionPtr = 0xABDE7C98;
-	//send the preamble
-	Comms_Device_SendPacket(commsDevice, &preamble, sizeof(preamble));
-	//receive the ACK_PREAMBLE RESPONSE
-	Comms_Device_RecvPacket(commsDevice, &ackPreambleResponse, sizeof(ackPreambleResponse));
-	if (ackPreambleResponse == ACK_PREAMBLE) {
+	if (run_preamble_sequence(commsDevice) == true) {
 		printf("The ACK_PREAMBLE Response host_bl_get_rdp_status has been read here.\n");
-		//send the length to follow: pkt[0]
-		Comms_Device_SendPacket(commsDevice, bl_get_rdp_status_pkt, sizeof(bl_get_rdp_status_pkt[0]));
-		//send the rest of pkt
-		Comms_Device_SendPacket(commsDevice, bl_get_rdp_status_pkt+1, sizeof(bl_get_rdp_status_pkt)-1);
-		//receive crcResponse pkt
-		Comms_Device_RecvPacket(commsDevice, crcResponse, 2);
-		printf("The CRC Response host_bl_get_rdp_status has been read here.\n");
-		if (crcResponse[0] == ACK) {
+		if (run_protocol_packet_sequence(commsDevice, bl_get_rdp_status_pkt, 6, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_get_rdp_status has been read here.\n");
 			//receive the boot loader version value
 			Comms_Device_RecvPacket(commsDevice, &mcu_flash_rdp_status, crcResponse[1]);
 			if (mcu_flash_rdp_status == 0xAA) {
@@ -129,26 +125,15 @@ static void bl_get_rdp_status (comms_device_t commsDevice) {
 static void bl_get_chip_id (comms_device_t commsDevice) {
 	uint8_t bl_get_chip_id_pkt[6];
 	uint8_t crcResponse[2];
-	uint8_t ackPreambleResponse = 0;
 	uint16_t MCU_CHIP_ID = 0xFFFF;
 	bl_get_chip_id_pkt[0] = 5;
 	bl_get_chip_id_pkt[1] = BL_GET_CID;
 	uint32_t * crcPortionPtr = (uint32_t *)(bl_get_chip_id_pkt+2);
 	*crcPortionPtr = 0xABDE7C98;
-	//send the preamble
-	Comms_Device_SendPacket(commsDevice, &preamble, sizeof(preamble));
-	//receive the ACK_PREAMBLE RESPONSE
-	Comms_Device_RecvPacket(commsDevice, &ackPreambleResponse, sizeof(ackPreambleResponse));
-	if (ackPreambleResponse == ACK_PREAMBLE) {
+	if (run_preamble_sequence(commsDevice) == true) {
 		printf("The ACK_PREAMBLE Response host_bl_get_chip_id has been read here.\n");
-		//send the length to follow: pkt[0]
-		Comms_Device_SendPacket(commsDevice, bl_get_chip_id_pkt, sizeof(bl_get_chip_id_pkt[0]));
-		//send the rest of pkt
-		Comms_Device_SendPacket(commsDevice, bl_get_chip_id_pkt+1, sizeof(bl_get_chip_id_pkt)-1);
-		//receive crcResponse pkt
-		Comms_Device_RecvPacket(commsDevice, crcResponse, 2);
-		printf("The CRC Response host_bl_get_chip_id has been read here.\n");
-		if (crcResponse[0] == ACK) {
+		if (run_protocol_packet_sequence(commsDevice, bl_get_chip_id_pkt, 6, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_get_chip_id has been read here.\n");
 			//receive the boot loader version value
 			Comms_Device_RecvPacket(commsDevice, &MCU_CHIP_ID, crcResponse[1]);
 			printf("The MCU Chip ID is: %x\n", MCU_CHIP_ID);
@@ -157,28 +142,17 @@ static void bl_get_chip_id (comms_device_t commsDevice) {
 }
 
 static void host_bl_get_help (comms_device_t commsDevice) {
-	uint8_t bl_get_version_pkt[6];
+	uint8_t bl_get_help_pkt[6];
 	uint8_t crcResponse[2];
-	uint8_t ackPreambleResponse = 0;
 	unsigned char  bootLoaderHelpMessage[50] = {0};
-	bl_get_version_pkt[0] = 5;
-	bl_get_version_pkt[1] = BL_GET_HELP;
-	uint32_t * crcPortionPtr = (uint32_t *)(bl_get_version_pkt+2);
+	bl_get_help_pkt[0] = 5;
+	bl_get_help_pkt[1] = BL_GET_HELP;
+	uint32_t * crcPortionPtr = (uint32_t *)(bl_get_help_pkt+2);
 	*crcPortionPtr = 0xABDE7C98;
-	//send the preamble
-	Comms_Device_SendPacket(commsDevice, &preamble, sizeof(preamble));
-	//receive the ACK_PREAMBLE RESPONSE
-	Comms_Device_RecvPacket(commsDevice, &ackPreambleResponse, sizeof(ackPreambleResponse));
-	if (ackPreambleResponse == ACK_PREAMBLE) {
+	if (run_preamble_sequence(commsDevice) == true) {
 		printf("The ACK_PREAMBLE Response host_bl_get_help has been read here.\n");
-		//send the length to follow: pkt[0]
-		Comms_Device_SendPacket(commsDevice, bl_get_version_pkt, sizeof(bl_get_version_pkt[0]));
-		//send the rest of pkt
-		Comms_Device_SendPacket(commsDevice, bl_get_version_pkt+1, sizeof(bl_get_version_pkt)-1);
-		//receive crcResponse pkt
-		Comms_Device_RecvPacket(commsDevice, crcResponse, 2);
-		printf("The CRC Response host_bl_get_help has been read here.\n");
-		if (crcResponse[0] == ACK) {
+		if (run_protocol_packet_sequence(commsDevice, bl_get_help_pkt, 6, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_get_help has been read here.\n");
 			//receive the boot loader version value
 			Comms_Device_RecvPacket(commsDevice, bootLoaderHelpMessage, crcResponse[1]);
 			printf("The MCU Boot Loader Help message is: %s\n", bootLoaderHelpMessage);
@@ -190,30 +164,89 @@ static void host_bl_get_version (comms_device_t commsDevice) {
 	uint8_t bl_get_version_pkt[6];
 	uint8_t crcResponse[2];
 	uint8_t bootLoaderVersion = 0xFF;
-	uint8_t ackPreambleResponse = 0;
 	bl_get_version_pkt[0] = 5;
 	bl_get_version_pkt[1] = BL_GET_VER;
 	uint32_t * crcPortionPtr = (uint32_t *)(bl_get_version_pkt+2);
 	*crcPortionPtr = 0xABDE7C98;
-	//send the preamble
-	Comms_Device_SendPacket(commsDevice, &preamble, sizeof(preamble));
-	//receive the ACK_PREAMBLE RESPONSE
-	Comms_Device_RecvPacket(commsDevice, &ackPreambleResponse, sizeof(ackPreambleResponse));
-	if (ackPreambleResponse == ACK_PREAMBLE) {
+	if (run_preamble_sequence(commsDevice) == true) {
 		printf("The ACK_PREAMBLE Response host_bl_get_version has been read here.\n");
-		//send the length to follow: pkt[0]
-		Comms_Device_SendPacket(commsDevice, bl_get_version_pkt, sizeof(bl_get_version_pkt[0]));
-		//send the rest of pkt
-		Comms_Device_SendPacket(commsDevice, bl_get_version_pkt+1, sizeof(bl_get_version_pkt)-1);
-		//receive crcResponse pkt
-		Comms_Device_RecvPacket(commsDevice, crcResponse, 2);
-		printf("The CRC Response host_bl_get_version has been read here.\n");
-		if (crcResponse[0] == ACK) {
+		if (run_protocol_packet_sequence(commsDevice, bl_get_version_pkt, 6, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_get_version has been read here.\n");
 			//receive the boot loader version value
 			Comms_Device_RecvPacket(commsDevice, &bootLoaderVersion, crcResponse[1]);
 			printf("The MCU Boot Loader version is: %x\n", bootLoaderVersion);
 		}
 	}
+}
+
+static void bl_go_to_app_address (comms_device_t commsDevice) {
+	uint8_t bl_go_to_app_address_pkt[10];
+	uint8_t crcResponse[2];
+	uint32_t address_goto = 0x08008000;
+	uint8_t commandStatus = 0;
+	bl_go_to_app_address_pkt[0] = 9;
+	bl_go_to_app_address_pkt[1] = BL_GO_TO_ADDR;
+	uint32_t * memory_address_value = (uint32_t *)(bl_go_to_app_address_pkt+2);
+	uint32_t * crcPortionPtr = (uint32_t *)(bl_go_to_app_address_pkt+6);
+	*crcPortionPtr = 0xABDE7C98;
+	*memory_address_value = address_goto;
+	if (run_preamble_sequence(commsDevice) == true) {
+		printf("The ACK_PREAMBLE Response host_bl_go_to_app_address has been read here.\n");
+		if (run_protocol_packet_sequence(commsDevice, bl_go_to_app_address_pkt, 10, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_go_to_app_address has been read here.\n");
+			//receive the app go-to-address status
+			Comms_Device_RecvPacket(commsDevice, &commandStatus, crcResponse[1]);
+			printf("The go-to-app-address command execution status is: %x\n", commandStatus);
+		}
+	}
+}
+
+static void bl_flash_erase (comms_device_t commsDevice) {
+	uint8_t bl_flash_erase_pkt[8];
+	uint8_t crcResponse[2];
+	uint8_t commandStatus = 0;
+	bl_flash_erase_pkt[0] = 7;
+	bl_flash_erase_pkt[1] = BL_FLASH_ERASE;
+	bl_flash_erase_pkt[2] = 2;
+	bl_flash_erase_pkt[3] = 4;
+	uint32_t * crcPortionPtr = (uint32_t *)(bl_flash_erase_pkt+4);
+	*crcPortionPtr = 0xABDE7C98;
+	if (run_preamble_sequence(commsDevice) == true) {
+		printf("The ACK_PREAMBLE Response host_bl_flash_erase has been read here.\n");
+		if (run_protocol_packet_sequence(commsDevice, bl_flash_erase_pkt, 8, crcResponse, 2) == true) {
+			printf("The CRC Response host_bl_flash_erase has been read here.\n");
+			//receive the app go-to-address status
+			Comms_Device_RecvPacket(commsDevice, &commandStatus, crcResponse[1]);
+			printf("The flash-erase command execution status is: %x\n", commandStatus);
+		}
+	}
+}
+
+static bool run_protocol_packet_sequence (comms_device_t commsDevice, uint8_t * packet,
+		uint32_t packet_size, uint8_t * crcResponse, uint32_t crcResponseSize) {
+	//send the length to follow: packet[0]
+	Comms_Device_SendPacket(commsDevice, packet, sizeof(packet[0]));
+	//send the rest of packet
+	Comms_Device_SendPacket(commsDevice, packet+1, packet_size-1);
+	//receive crcResponse packet
+	Comms_Device_RecvPacket(commsDevice, crcResponse, crcResponseSize);
+
+	if (crcResponse[0] == ACK) {
+		return true;
+	}
+	return false;
+}
+
+static bool run_preamble_sequence (comms_device_t commsDevice) {
+	uint8_t ackPreambleResponse = 0;
+	//send the preamble
+	Comms_Device_SendPacket(commsDevice, &preamble, sizeof(preamble));
+	//receive the ACK_PREAMBLE RESPONSE
+	Comms_Device_RecvPacket(commsDevice, &ackPreambleResponse, sizeof(ackPreambleResponse));
+	if (ackPreambleResponse == ACK_PREAMBLE) {
+		return true;
+	}
+	return false;
 }
 
 static void exitHostBootloader (void)
